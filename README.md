58f00b24819d14d47a13f9e1cc5ef20e1c7db476
Nauman <24100311@lums.edu.pk>
2024-10-08 16:02:04 +0500

    add readme + screenshots

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Teen Patti
+# TeenPatti Project
+
+## Project Details
+
+The **TeenPatti** project is a card game developed as part of a Computer Science 300 course using React and Socket.IO for real-time communication. TeenPatti, often referred to as "Indian Poker," is a three-card gambling game widely popular in South Asia. This project serves as an implementation of the game, incorporating key features like player registration, card shuffling, and betting.
+
+The repository is organized into front-end and back-end components:
+
+- **Front-end:** Built using React with TypeScript, styled with CSS.
+- **Back-end:** Handled server-side logic for the game using Socket.IO and potentially Node.js.
+
+### Key Features
+
+- Real-time multiplayer gameplay using **Socket.IO** for communication between players and the server.
+- Game states managed through React components
+- Real-time updates and dynamic interactions using JavaScript/TypeScript
+- User-friendly interface with game rules and betting logic integrated
+
+## Goals
+
+- Provide an academic submission for a CS300 course while demonstrating skills in full-stack development.
+- Create a functional, interactive, and real-time TeenPatti game for multiple players.
+- Utilize modern JavaScript technologies like React and TypeScript for front-end development.
+- Implement real-time socket communication using **Socket.IO**.
+- Experiment with game logic and real-time state management within a web application.
+
+## Screenshots of the Application
+
+![Homepage](Screenshots/homePage.png)  
+_Login page where users can enter their username to join the game._
+
+![Gamepage](Screenshots/gameScreen.png)  
+_Game page where all players are displayed at the table. Players can view messages, their cards, and the chips available._
+
+## Steps to Run the Project:
+
+1. Clone the repository to your local machine:
+   `git clone https://github.com/naumanijazch/TeenPatti.git `
+2. Navigate to project directory:
+   `cd TeenPatti`
+3. Frontend:
+   - Navigate to frontend directory: `cd frontend`
+   - Install the necessary dependencies: `npm install`
+   - Start the development server: `npm start`
+4. Backend:
+   - Navigate to backend directory: `cd backend`
+   - Start the backend server: `ts-node server.ts`
--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,59 +1,54 @@
-import { Socket } from "socket.io-client" 
-import { DefaultEventsMap } from "socket.io/dist/typed-events"
-import './Home.css'
-import React, {useState, useEffect} from 'react'
-import { useNavigate } from 'react-router-dom';
+import { Socket } from "socket.io-client";
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
+import "./Home.css";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 //create an interface for the props that you want to pass to this component
 interface HomePageProps {
-    socket: Socket<DefaultEventsMap, DefaultEventsMap> //this is the type for sockets 
-    //you can always add more functions/objects that you would like as props for this component
+  socket: Socket<DefaultEventsMap, DefaultEventsMap>; //this is the type for sockets
+  //you can always add more functions/objects that you would like as props for this component
 }
 
+function HomePage({ socket }: HomePageProps) {
+  //click handler
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    socket.on("UsersFull", (playerNames: any) => {
+      console.log("USERSS FULLLL", playerNames);
+      navigate("/TeenPatti", { state: { playerNames: playerNames } });
+    });
 
-function HomePage({socket}:HomePageProps){
-    //click handler
-    const [username, setUsername] = useState('')       
-    const navigate = useNavigate();
-    
+    socket.on("MaxPlayers", () => {
+      console.log("Redict");
+      navigate("/tooManyUsers");
+    });
+  }, [socket]);
 
-    useEffect(() => {
-        socket.on("UsersFull", (playerNames:any) => {
-          console.log("USERSS FULLLL", playerNames)
-          navigate('/TeenPatti', {state: {playerNames: playerNames}})
-        });
-        
-        socket.on("MaxPlayers", () => {
-            console.log("Redict")
-            navigate('/tooManyUsers')
-        });
+  const handleClick = (socket: Socket) => {
+    console.log("Socket ID:", socket.id);
+    socket.emit("newPlayer", { username: username });
+    navigate("/waitingScreen");
+  };
 
-    }, [socket]);
-      
+  const settingUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
 
-    const handleClick = (socket: Socket) => {
-        console.log('Socket ID:', socket.id);
-        socket.emit("newPlayer", { username: username })
-        navigate('/waitingScreen')
-    };
-
-    const settingUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(e.target.value)
-    }
-
-
-    return(
-        <>
-        <div className="sampleHomePage">                
-            <h1 className="sampleTitle">Welcome to TeenPatti</h1>
-            <div className="sampleMessage">
-                <input  placeholder = "enter username" value={username} onChange={settingUsername}></input>
-                <button  onClick={() => handleClick(socket)}>Join Game</button>
-            </div>        
-        </div>
-        </>
-    )
-
+  return (
+    <div className="sampleHomePage">
+      <h1 className="sampleTitle">Welcome to TeenPatti</h1>
+      <div className="sampleMessage">
+        <input
+          placeholder="enter username"
+          value={username}
+          onChange={settingUsername}
+        ></input>
+        <button onClick={() => handleClick(socket)}>Join Game</button>
+      </div>
+    </div>
+  );
 }
-export default HomePage
+export default HomePage;
